23c1cdd6b313986233bca0bf003f59a99128357b
Francisco <franciscolunademoraes@gmail.com>
2026-09-10 23:28:18 -0300

    rascunho classes

--- Pratica/Aula_07/Luta.java
+++ Pratica/Aula_07/Luta.java
@@ -1,4 +1,10 @@
 package Pratica.Aula_07;
 
 public class Luta {
+
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovada;
+
 }
--- Pratica/Aula_07/Lutador.java
+++ Pratica/Aula_07/Lutador.java
@@ -1,4 +1,18 @@
 package Pratica.Aula_07;
 
 public class Lutador {
+
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    //construtor
+
+
 }
