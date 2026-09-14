fd67139fcf5982c1834e9a203bae7035accb3962
Francisco <franciscolunademoraes@gmail.com>
2026-09-14 20:22:45 -0300

    criação do construtor e termino dos metodos getters e setters

--- Pratica/Aula_07/Lutador.java
+++ Pratica/Aula_07/Lutador.java
@@ -13,6 +13,26 @@ public class Lutador {
     private int empates;
 
     //construtor
+    public Lutador
+                   (String nome,
+                   String nacionalidade,
+                   int idade,
+                   float altura,
+                   float peso,
+                   int vitorias,
+                   int derrotas,
+                   int empates) {
+
+        setNome(nome);
+        setNacionalidade(nacionalidade);
+        setIdade(idade);
+        setAltura(altura);
+        setPeso(peso);
+        setVitorias(vitorias);
+        setDerrotas(derrotas);
+        setEmpates(empates);
+
+    }
 
 
     //getters e setters
@@ -36,6 +56,59 @@ public class Lutador {
     public int getIdade() {
         return this.idade;
     }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    private void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getVitorias() {
+        return this.vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return this.derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return this.empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
 
 
     //metodos classe
